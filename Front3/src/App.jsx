9e63bbe29b1c9a0ee70a298d6cabea933347bfb6
Maria-Cavallo <you@example.com>
2022-11-24 08:25:03 -0300

    no anda

--- Front3/src/App.jsx
+++ Front3/src/App.jsx
@@ -1,13 +1,7 @@
 import React, { Fragment, useState } from 'react'
-// import Keys from './components/week_2/class_2/Keys'
-// import Main from './components/week_2/class_3/Main'
-// import LocalApp from './components/week_3/LocalApp'
-// import FormComponent from './components/week_3/class_2/FormComponent'
-//ACTIVIDAD DE REPASO -- TODOS => import Dashboard from './components/week_4/class_1/Dashboard'
-//import Store from './components/week_4/class_3/Store'
-import ClassComponent from './components/week_5/class_1/ClassComponent'
-import FunctionComponent from './components/week_5/class_2/FunctionComponent';
 import Playlist from './components/week_5/class_3/Playlist';
-const App = () => <Playlist />
 
+const App = () => {
+    <Playlist />
+}
 export default App
--- Front3/src/main.jsx
+++ Front3/src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppRouter'
+import App from './App'
 import './index.css'
-//import Index from './components/PARCIAL/app'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
